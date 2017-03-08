d78e8d90e0ed0c99779d7c636d8f4b963da82fd8
Maslintsyn Ivan <maslintsyn@gmail.com>
2017-03-08 15:42:45 +0300

    Масштабирование поля

--- GUI.py
+++ GUI.py
@@ -2,31 +2,32 @@ import sys
 from PyQt5 import QtWidgets
 from PyQt5 import QtCore
 from PyQt5.QtGui import QBrush
-from PyQt5.QtGui import QPainter
 from PyQt5.QtGui import QColor
 from PyQt5.QtGui import QIcon
 from PyQt5.QtGui import QPen
 from PyQt5.QtWidgets import QMenu
 from PyQt5.QtGui import QKeySequence
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QCursor
 
 import main
 
 
 class Settings:
 
-    WIDTH = 50
-    HEIGHT = 50
-    NUM_BLOCKS_X = 5
-    NUM_BLOCKS_Y = 5
-    INDENTATION = 2
+    width = 50
+    height = 50
+    num_blocks_x = 10
+    num_blocks_y = 10
+    indentation = 2
+    table_size_x = width * num_blocks_x
+    table_size_y = height * num_blocks_y
 
 
 def create_action(parent, text, slot=None,
                   shortcut=None, shortcuts=None, shortcut_context=None,
                   icon=None, tooltip=None,
                   checkable=False, checked=False):
+
     action = QtWidgets.QAction(text, parent)
 
     if icon is not None:
@@ -54,8 +55,7 @@ class QS(QtWidgets.QGraphicsScene):
 
     def __init__(self, *args):
         super().__init__(*args)
-
-        self.Table = main.Game(Settings.NUM_BLOCKS_X, Settings.NUM_BLOCKS_Y)
+        self.Table = main.Game(Settings.num_blocks_x, Settings.num_blocks_y)
         self.lines = []
         self.draw_grid()
         self.set_opacity(0.1)
@@ -69,31 +69,29 @@ class QS(QtWidgets.QGraphicsScene):
             brush = QBrush(QtCore.Qt.black)
         else:
             brush = QBrush(QtCore.Qt.white)
-        X = Settings.WIDTH * x + Settings.INDENTATION
-        Y = Settings.HEIGHT * y + Settings.INDENTATION
-        self.addRect(X, Y, Settings.WIDTH-2*Settings.INDENTATION, Settings.HEIGHT-2*Settings.INDENTATION, pen, brush)
+        x = Settings.width * x + Settings.indentation
+        y = Settings.height * y + Settings.indentation
+        self.addRect(x, y, Settings.width-2*Settings.indentation, Settings.height-2*Settings.indentation, pen, brush)
 
     def draw_cells(self):
-        for x in range(Settings.NUM_BLOCKS_X):
-            for y in range(Settings.NUM_BLOCKS_Y):
+        for x in range(Settings.num_blocks_x):
+            for y in range(Settings.num_blocks_y):
                 self.draw_cell(x, y, self.Table.get(x, y).color)
 
-
-
     def draw_grid(self):
-        width = Settings.NUM_BLOCKS_X * Settings.WIDTH
-        height = Settings.NUM_BLOCKS_Y * Settings.HEIGHT
+        width = Settings.num_blocks_x * Settings.width
+        height = Settings.num_blocks_y * Settings.height
         self.setSceneRect(0, 0, width, height)
         self.setItemIndexMethod(QtWidgets.QGraphicsScene.NoIndex)
 
         pen = QPen(QColor(0, 0, 0), 1, Qt.SolidLine)
 
-        for x in range(0, Settings.NUM_BLOCKS_X+1):
-            xc = x * Settings.WIDTH
+        for x in range(0, Settings.num_blocks_x+1):
+            xc = x * Settings.width
             self.lines.append(self.addLine(xc, 0, xc, height, pen))
 
-        for y in range(0, Settings.NUM_BLOCKS_Y+1):
-            yc = y * Settings.HEIGHT
+        for y in range(0, Settings.num_blocks_y+1):
+            yc = y * Settings.height
             self.lines.append(self.addLine(0, yc, width, yc, pen))
 
     def set_visible(self, visible=True):
@@ -114,6 +112,8 @@ class QV(QtWidgets.QGraphicsView):
 
     def __init__(self, *args):
         super().__init__(*args)
+
+        # self.setAlignment(QtCore.Qt.AlignTop | QtCore.Qt.AlignLeft ) # зафиксировать клетку в верхнем левом угле
 
         self.view_menu = QMenu(self)
         self.create_actions()
@@ -143,20 +143,28 @@ class QV(QtWidgets.QGraphicsView):
         self.scale(1.0 / 1.5, 1.0 / 1.5)
 
     def mousePressEvent(self, event):
-        cell_x = event.x()//Settings.WIDTH
-        cell_y = event.y()//Settings.HEIGHT
-        if 0 <= cell_x <= Settings.NUM_BLOCKS_X and 0 <= cell_y <= Settings.NUM_BLOCKS_Y:
-            print(cell_x, cell_y)
-            # print(self.scale())
+
+        point = self.mapToScene(event.x(), event.y())
+
+        cell_x = int(point.x()//Settings.width)
+        cell_y = int(point.y()//Settings.height)
+        if 0 <= cell_x <= Settings.num_blocks_x and 0 <= cell_y <= Settings.num_blocks_y:
+
             if event.button() == Qt.LeftButton:
                 a.Table.turn(cell_x, cell_y)
-                #print(a.Table)
                 a.draw_cells()
-                #a.draw_cell(cell_x,cell_y)
+
+                if self.is_win():
+                    print("You Win", a.Table.NumberOfTurn)
+                    pass
+
             else:
                 return
         else:
             return
+
+    def is_win(self):
+        return len(set(str(a.Table))) <= 2
 
 
 if __name__ == '__main__':
@@ -164,6 +172,5 @@ if __name__ == '__main__':
     a = QS()
     b = QV()
     b.setScene(a)
-    # b.resize(800,600)
     b.show()
     sys.exit(app.exec_())
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 class Game:
     def __init__(self, size_x, size_y = False, presetting=False):
         """
@@ -11,18 +13,25 @@ class Game:
         if presetting:
             self.field = presetting
         else:
-            self.field = [[Cells() for _ in range(size_x)] for _ in range(size_y)]
+            self.field = [[Cells(random.randint(0,1)) for _ in range(size_y)] for _ in range(size_x)]
 
         self.size_x = size_x
         self.size_y = size_y
         self.NumberOfTurn = 0
 
     def turn(self, x, y):
+        """
 
+        :param x: х-овая координата
+        :param y: у-овая координата
+        :return: пересчет поля с учетом хода. перекрашиваем соседние по грани клетки
+        """
+        if 0 <= x < self.size_x and 0 <= y <= self.size_y:
+            self.field[x][y].change_color()
+            self.NumberOfTurn += 1
+        else:
+            return
 
-
-        self.NumberOfTurn += 1
-        self.field[x][y].change_color()
         if 0 <= (x - 1) < self.size_x:
             self.field[x-1][y].change_color()
         if 0 <= (x + 1) < self.size_x:
@@ -38,11 +47,22 @@ class Game:
         return "\n".join(["".join(map(str, i)) for i in self.field])
 
     def get(self, x, y):
+        """
+
+        :param x: х-овая координата
+        :param y: у-овая координата
+        :return: возращяем цвет клетки (1 - черный, 0 - белый)
+        """
+
         return self.field[x][y]
 
 
 class Cells:
     def __init__(self, color=1):
+        """
+
+        :param color: (1 - черный, 0 - белый)
+        """
         self.color = color
 
     def color(self):
@@ -57,7 +77,5 @@ class Cells:
 
 if __name__ == "__main__":
     Table = Game(5,6)
-    Table.turn(0, 0)
-    print(Table.get(0, 0))
     print(Table)
     # C2 = Cells(color=0)
