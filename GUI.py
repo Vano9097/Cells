334deb5f44b0db268e28e19f75bc8be1060cbfc5
Maslintsyn Ivan <maslintsyn@gmail.com>
2017-03-19 10:12:34 +0300

    Меню

--- GUI.py
+++ GUI.py
@@ -1,11 +1,11 @@
-
 from PyQt5 import QtWidgets
 from PyQt5 import QtCore
 from PyQt5.QtGui import QBrush
 from PyQt5.QtGui import QColor
 from PyQt5.QtGui import QIcon
 from PyQt5.QtGui import QPen
-from PyQt5.QtWidgets import QMenu
+from PyQt5.QtWidgets import QAction, qApp
+from PyQt5.QtWidgets import QMenu, QMainWindow
 from PyQt5.QtGui import QKeySequence
 from PyQt5.QtCore import Qt
 
@@ -20,7 +20,6 @@ def create_action(parent, text, slot=None,
                   shortcut=None, shortcuts=None, shortcut_context=None,
                   icon=None, tooltip=None,
                   checkable=False, checked=False):
-
     action = QtWidgets.QAction(text, parent)
 
     if icon is not None:
@@ -39,13 +38,12 @@ def create_action(parent, text, slot=None,
     if checked:
         action.setChecked(True)
     if slot is not None:
-        action.triggered.connect(slot) # ??
+        action.triggered.connect(slot)  # ??
 
     return action
 
 
 class QS(QtWidgets.QGraphicsScene):
-
     def __init__(self, *args):
         super().__init__(*args)
         self.Table = main.Game(Settings.num_blocks_x, Settings.num_blocks_y)
@@ -63,7 +61,8 @@ class QS(QtWidgets.QGraphicsScene):
             brush = QBrush(QtCore.Qt.white)
         x = Settings.width * x + Settings.indentation
         y = Settings.height * y + Settings.indentation
-        self.addRect(x, y, Settings.width-2*Settings.indentation, Settings.height-2*Settings.indentation, pen, brush)
+        self.addRect(x, y, Settings.width - 2 * Settings.indentation, Settings.height - 2 * Settings.indentation, pen,
+                     brush)
 
     def draw_cells(self):
         for x in range(Settings.num_blocks_x):
@@ -78,11 +77,11 @@ class QS(QtWidgets.QGraphicsScene):
 
         pen = QPen(QColor(0, 0, 0), 1, Qt.SolidLine)
 
-        for x in range(0, Settings.num_blocks_x+1):
+        for x in range(0, Settings.num_blocks_x + 1):
             xc = x * Settings.width
             self.lines.append(self.addLine(xc, 0, xc, height, pen))
 
-        for y in range(0, Settings.num_blocks_y+1):
+        for y in range(0, Settings.num_blocks_y + 1):
             yc = y * Settings.height
             self.lines.append(self.addLine(0, yc, width, yc, pen))
 
@@ -101,9 +100,10 @@ class QS(QtWidgets.QGraphicsScene):
 
 
 class QV(QtWidgets.QGraphicsView):
-
     def __init__(self, *args):
         super().__init__(*args)
+        self.a = QS()
+        self.setScene(self.a)
 
         # self.setAlignment(QtCore.Qt.AlignTop | QtCore.Qt.AlignLeft ) # зафиксировать клетку в верхнем левом угле
 
@@ -111,6 +111,7 @@ class QV(QtWidgets.QGraphicsView):
         self.create_actions()
 
     def create_actions(self):
+
         act = create_action(self.view_menu, "Zoom in",
                             slot=self.on_zoom_in,
                             shortcut=QKeySequence("+"), shortcut_context=Qt.WidgetShortcut)
@@ -138,16 +139,17 @@ class QV(QtWidgets.QGraphicsView):
 
         point = self.mapToScene(event.x(), event.y())
 
-        cell_x = int(point.x()//Settings.width)
-        cell_y = int(point.y()//Settings.height)
+        cell_x = int(point.x() // Settings.width)
+        cell_y = int(point.y() // Settings.height)
+
         if 0 <= cell_x <= Settings.num_blocks_x and 0 <= cell_y <= Settings.num_blocks_y:
 
             if event.button() == Qt.LeftButton:
-                a.Table.turn(cell_x, cell_y)
-                a.draw_cells()
+                self.a.Table.turn(cell_x, cell_y)
+                self.a.draw_cells()
 
                 if self.is_win():
-                    print("You Win", a.Table.NumberOfTurn)
+                    print("You Win", self.a.Table.NumberOfTurn)
                     pass
 
             else:
@@ -156,13 +158,62 @@ class QV(QtWidgets.QGraphicsView):
             return
 
     def is_win(self):
-        return ("0" not in set(str(a.Table))) or ("1" not in set(str(a.Table)))
+        return ("0" not in set(str(self.a.Table))) or ("1" not in set(str(self.a.Table)))
+
+
+class Window(QMainWindow):
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.b = QV()
+        self.setCentralWidget(self.b)
+        self.menu = self.menuBar()
+        self.create_menu()
+
+    def create_menu(self):
+        self.file = self.menu.addMenu("Файл")
+        new_random = create_action(self.menu, "Случайный", slot=None,
+                                   shortcut=None, shortcuts=None, shortcut_context=None,
+                                   icon=None, tooltip=None,
+                                   checkable=False, checked=False)
+        self.file.addAction(new_random)
+
+        action_open = create_action(self.menu, "Открыть", slot=qApp.quit(),
+                                    shortcut=None, shortcuts=None, shortcut_context=None,
+                                    icon=None, tooltip=None,
+                                    checkable=False, checked=False)
+        self.file.addAction(action_open)
+
+        self.file.addSeparator()
+
+        action_save = create_action(self.menu, "Сохранить", slot=None,
+                                    shortcut=None, shortcuts=None, shortcut_context=None,
+                                    icon=None, tooltip=None,
+                                    checkable=False, checked=False)
+        self.file.addAction(action_save)
+
+        self.file.addSeparator()
+
+        action_settings = create_action(self.menu, "Настройки", slot=None,
+                                        shortcut=None, shortcuts=None, shortcut_context=None,
+                                        icon=None, tooltip=None,
+                                        checkable=False, checked=False)
+        self.file.addAction(action_settings)
+
+        self.file.addSeparator()
+
+        action_exit = create_action(self.menu, "Выход", slot=None,
+                                    shortcut=None, shortcuts=None, shortcut_context=None,
+                                    icon=None, tooltip=None,
+                                    checkable=False, checked=False)
+        self.file.addAction(action_exit)
+
+        self.information = self.menu.addMenu("Справка")
+
+        self.setMenuBar(self.menu)
 
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
-    a = QS()
-    b = QV()
-    b.setScene(a)
-    b.show()
+    c = Window()
+    c.show()
     sys.exit(app.exec_())
